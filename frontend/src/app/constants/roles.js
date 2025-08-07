3ca93a63e68e2ad7931d1ddc7ec37131f63bcb9c
Nathan Ong <nathanong1217@gmail.com>
2025-08-07 12:45:29 -0700

    added roles utility

---
+++ frontend/src/app/constants/roles.js
@@ -0,0 +1,28 @@
+export const ROLES = {
+  IT: "IT",
+  Director: "Director",
+  USER: "User",
+  PM: "PropertyManager",
+  Broker: "Broker",
+};
+
+
+export function hasRole(session, role) {
+  return session?.user?.roles?.includes(role);
+}
+
+export function isIT(session) {
+  return hasRole(session, ROLES.IT);
+}
+
+export function isDirector(session) {
+  return hasRole(session, ROLES.Director);
+}
+
+export function isPM(session) {
+  return hasRole(session, ROLES.PM);
+}
+
+export function isBroker(session) {
+  return hasRole(session, ROLES.Broker);
+}
